183287542b0dfb96da7d8975aec651b351a152b8
krudoppler <bkru33@gmail.com>
2025-05-03 18:54:09 -0500

    push

--- travel_journal_react/src/components/header.jsx
+++ travel_journal_react/src/components/header.jsx
@@ -1,9 +1,10 @@
-export default Header() {
-    return (
-        <>
-        <h1>    test</h1>
-        
-        </>
-       
-    )
+export default function Header() {
+  return (
+    <header class="main-header">
+      <div class="header-content">
+        <img src="/src/assets/globe.png" class="globe-img" alt="Globe icon" />
+        <h3>my travel journal.</h3>
+      </div>
+    </header>
+  );
 }
---
+++ travel_journal_react/src/components/maincontent.jsx
@@ -0,0 +1,5 @@
+export default function MainContent(){
+    return (
+        <p>This is the main content</p>
+    )
+}
