465595273dfe11c68fda7e42901e9af54112c0ac
krudoppler <bkru33@gmail.com>
2025-05-16 20:03:43 -0500

    push

--- travel_journal_react/src/components/maincontent.jsx
+++ travel_journal_react/src/components/maincontent.jsx
@@ -1,25 +1,18 @@
-export default function Entry() {
+export default function Entry(props) {
   return (
     <article className="entry-overall-container">
       <div className="entry-img">
-        <img
-          src="https://scrimba.com/links/travel-journal-japan-image-url"
-          alt="image of mount fuji"
-        />
+        <img src={props.img.src} alt={props.img.alt} />
       </div>
       <div className="entry-text">
         <p className="text-location">
           <img src="src/assets/marker.png" alt="marker" />
-          <span> JAPAN </span>
-          <a href="#">View on Google Maps</a>
+          <span> {props.country} </span>
+          <a href={props.googleMapsLink}>View on Google Maps</a>
         </p>
-        <h1 className="text-title">Mount Fuji</h1>
-        <h3 className="text-date"> 12 Jan, 2021 - 24 Jan, 2024</h3>
-        <p className="text-description">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <h1 className="text-title">{props.title}</h1>
+        <h3 className="text-date"> {props.date}</h3>
+        <p className="text-description">{props.text}</p>
       </div>
     </article>
   );
